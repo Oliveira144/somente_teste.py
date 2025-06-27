033b040e9c624495c5e05a3dc3957594e0b2f7bc
Oliveira144 <adm.oliveira144@gmail.com>
2025-06-27 12:52:58 -0300

    Update nao_usar.py

--- nao_usar.py
+++ nao_usar.py
@@ -1,298 +1,224 @@
-import streamlit as st
-from statistics import mean
-import random
 
-st.set_page_config(page_title="Football Studio Pro", layout="centered")
+importar streamlit como st
+importar aleatório
 
-st.markdown(
-    """
-    <style>
-        .result-bubble {
-            width: 40px;
-            height: 40px;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            border-radius: 50%;
-            font-weight: bold;
-            font-size: 16px;
-            margin: 2px;
-        }
-    </style>
-    """, unsafe_allow_html=True
-)
+st.set_page_config(page_title="Analisador de Estúdio de Futebol", layout="centralizado")
 
-# Inicializa sessões
-if 'history' not in st.session_state:
-    st.session_state.history = []
-if 'streak' not in st.session_state:
-    st.session_state.streak = {'type': None, 'count': 0}
-if 'suggestion' not in st.session_state:
-    st.session_state.suggestion = None
+# ---------- Função para análise dos 13 padrões ----------
+def analisar_padroes(resultados):
+    se len(resultados) < 3:
+        retornar Nenhum
 
+    recentes = resultados[-10:]
+    ultimos3 = resultados[-3:]
+    ultimos5 = resultados[-5:]
+    ultimos7 = resultados[-7:]
 
-def analyze_patterns(results):
-    if len(results) < 3:
-        return None
+    surf_detectado = Falso
+    sequência_atual = 1
+    para i no intervalo(len(recentes) - 2, -1, -1):
+        if recentes[i] == recentes[-1] e recentes[i] != "Empate":
+            sequência_atual += 1
+        outro:
+            quebrar
+    se current_streak >= 3:
+        surf_detectado = Verdadeiro
 
-    recent = results[-10:]
-    last3 = results[-3:]
-    last5 = results[-5:]
-    last7 = results[-7:]
+    contagem_em_ziguezague = 0
+    para i no intervalo(1, len(ultimos5)):
+        if ultimos5[i] != ultimos5[i-1] e ultimos5[i] != "Empate" e ultimos5[i-1] != "Empate":
+            contagem_em_ziguezague += 1
+    zigzag_detectado = contagem_zigzag >= 3
 
-    surfDetected = False
-    currentStreak = 1
-    for i in range(len(recent) - 2, -1, -1):
-        if recent[i] == recent[-1] and recent[i] != 'Empate':
-            currentStreak += 1
-        else:
-            break
-    if currentStreak >= 3:
-        surfDetected = True
+    duplas_repetidas = False
+    se len(recentes) >= 6:
+        dupla_count = 0
+        para i no intervalo(0, len(recentes) - 1, 2):
+            if recentes[i] == recentes[i + 1] e recentes[i] != "Empate":
+                contagem_dupla += 1
+        duplas_repetidas = dupla_count >= 2
 
-    zigzagCount = 0
-    for i in range(1, len(last5)):
-        if last5[i] != last5[i - 1] and last5[i] != 'Empate' and last5[i - 1] != 'Empate':
-            zigzagCount += 1
-    zigzagDetected = zigzagCount >= 3
-
-    duplasRepetidas = False
-    if len(recent) >= 6:
-        duplaCount = 0
-        for i in range(0, len(recent) - 1, 2):
-            if recent[i] == recent[i + 1] and recent[i] != 'Empate':
-                duplaCount += 1
-        duplasRepetidas = duplaCount >= 2
-
-    empatePositions = [i for i, r in enumerate(results) if r == 'Empate']
-    empateRecorrente = False
-    if len(empatePositions) >= 2:
-        lastEmpateGap = len(results) - 1 - empatePositions[-1]
-        if 15 <= lastEmpateGap <= 35:
-            empateRecorrente = True
+    pos_empates = [i para i, r em enumerate(resultados) se r == "Empate"]
+    empate_recorrente = False
+    se len(pos_empates) >= 2:
+        intervalo = len(resultados) - 1 - pos_empates[-1]
+        se 15 <= intervalo <= 35:
+            empate_recorrente = True
 
     padrao_escada = False
-    if len(recent) >= 6:
-        escalones = []
-        count = 1
-        current = recent[0]
-        for i in range(1, len(recent)):
-            if recent[i] == current and recent[i] != 'Empate':
-                count += 1
-            else:
-                if current != 'Empate':
-                    escalones.append(count)
-                current = recent[i]
-                count = 1
-        if current != 'Empate':
-            escalones.append(count)
-        for i in range(len(escalones) - 2):
-            if escalones[i + 1] == escalones[i] + 1 and escalones[i + 2] == escalones[i] + 2:
-                padrao_escada = True
-                break
-
-    espelhamento = False
-    if len(last5) >= 4:
-        for i in range(len(last5) - 3):
-            seg = last5[i:i + 4]
-            if seg[0] == seg[3] and seg[1] == seg[2] and seg[0] != seg[1] and seg[0] != 'Empate' and seg[1] != 'Empate':
-                espelhamento = True
-                break
-
-    alternanciaEmpate = False
-    if len(last5) >= 3:
-        for i in range(len(last5) - 2):
-            if last5[i] != 'Empate' and last5[i + 1] == 'Empate' and last5[i + 2] != 'Empate' and last5[i] != last5[i + 2]:
-                alternanciaEmpate = True
-                break
-
-    padrao_onda = False
-    if len(recent) >= 6:
+    se len(recentes) >= 6:
         grupos = []
-        count = 1
-        current = recent[0]
-        for i in range(1, len(recent)):
-            if recent[i] == current and recent[i] != 'Empate':
-                count += 1
-            else:
-                if current != 'Empate':
-                    grupos.append(count)
-                current = recent[i]
-                count = 1
-        if current != 'Empate':
-            grupos.append(count)
-        for i in range(len(grupos) - 3):
-            if grupos[i:i + 4] == [1, 2, 1, 2]:
-                padrao_onda = True
-                break
+        contagem = 1
+        atual = recentes[0]
+        para i em intervalo(1, len(recentes)):
+            if recentes[i] == atual e recentes[i] != "Empate":
+                contagem += 1
+            outro:
+                se atual != "Empate":
+                    grupos.append(contagem)
+                atual = recentes[i]
+                contagem = 1
+        se atual != "Empate":
+            grupos.append(contagem)
+        para i no intervalo(len(grupos) - 2):
+            se grupos[i+1] == grupos[i] + 1 e grupos[i+2] == grupos[i] + 2:
+                padrao_escada = True
+                quebrar
 
-    padrao_3x1 = False
-    if len(recent) >= 4:
-        for i in range(len(recent) - 3):
-            seg = recent[i:i + 4]
-            if seg[0] == seg[1] == seg[2] and seg[2] != seg[3] and seg[0] != 'Empate' and seg[3] != 'Empate':
-                padrao_3x1 = True
-                break
+    espelhamento = Falso
+    se len(ultimos5) >= 4:
+        para i no intervalo(len(ultimos5) - 3):
+            seg = ultimos5[i:i+4]
+            if seg[0] == seg[3] e seg[1] == seg[2] e seg[0] != seg[1] e seg[0] != "Empate" e seg[1] != "Empate":
+                espelhamento = Verdadeiro
+                quebrar
 
-    padrao_3x3 = False
-    if len(recent) >= 6:
-        for i in range(len(recent) - 5):
-            seg = recent[i:i + 6]
-            if seg[0] == seg[1] == seg[2] and seg[3] == seg[4] == seg[5] and seg[0] != seg[3] and seg[0] != 'Empate' and seg[3] != 'Empate':
-                padrao_3x3 = True
-                break
+    alternancia_empate = False
+    se len(ultimos5) >= 3:
+        para i no intervalo(len(ultimos5) - 2):
+            if ultimos5[i] != "Empate" e ultimos5[i+1] == "Empate" e ultimos5[i+2] != "Empate" e ultimos5[i] != ultimos5[i+2]:
+                alternancia_empate = Verdadeiro
+                quebrar
 
-    empatesPontosFixes = False
-    if len(empatePositions) >= 2:
-        gaps = [empatePositions[i] - empatePositions[i - 1] for i in range(1, len(empatePositions))]
-        avgGap = mean(gaps)
-        if 9 <= avgGap <= 10:
-            empatesPontosFixes = True
+    padrao_onda = Falso
+    se len(recentes) >= 6:
+        grupos = []
+        contagem = 1
+        atual = recentes[0]
+        para i em intervalo(1, len(recentes)):
+            if recentes[i] == atual e recentes[i] != "Empate":
+                contagem += 1
+            outro:
+                se atual != "Empate":
+                    grupos.append(contagem)
+                atual = recentes[i]
+                contagem = 1
+        se atual != "Empate":
+            grupos.append(contagem)
+        para i no intervalo(len(grupos) - 3):
+            se grupos[i:i+4] == [1,2,1,2]:
+                padrao_onda = Verdadeiro
+                quebrar
 
-    quebrarSurf = surfDetected and currentStreak >= 4
-    quebrarZigzag = zigzagDetected and all(r != 'Empate' for r in last3)
-    quebrarDuplas = duplasRepetidas
+    padrao_3x1 = Falso
+    se len(recentes) >= 4:
+        para i no intervalo(len(recentes) - 3):
+            seg = recentes[i:i+4]
+            if seg[0] == seg[1] == seg[2] e seg[2] != seg[3] e seg[0] != "Empate" e seg[3] != "Empate":
+                padrao_3x1 = Verdadeiro
+                quebrar
 
-    casaCount = recent.count('Casa')
-    visitanteCount = recent.count('Visitante')
-    empateCount = recent.count('Empate')
+    padrao_3x3 = Falso
+    se len(recentes) >= 6:
+        para i no intervalo(len(recentes) - 5):
+            seg = recentes[i:i+6]
+            if seg[0] == seg[1] == seg[2] e seg[3] == seg[4] == seg[5] e seg[0] != seg[3] e seg[0] != "Empate" e seg[3] != "Empate":
+                padrao_3x3 = Verdadeiro
+                quebrar
 
-    suggestedEntry = None
-    conf = 0
-    mainPattern = ''
+    empates_fixos = False
+    se len(pos_empates) >= 2:
+        lacunas = [pos_empates[i] - pos_empates[i - 1] para i no intervalo(1, len(pos_empates))]
+        mídia = soma(lacunas) / len(lacunas)
+        se 9 <= media <= 10:
+            empates_fixos = True
 
-    if quebrarSurf:
-        currentColor = recent[-1]
-        suggestedEntry = 'Visitante' if currentColor == 'Casa' else 'Casa'
-        conf = 88 + min(currentStreak * 2, 10)
-        mainPattern = 'Quebra de Surf'
-    elif empateRecorrente:
-        suggestedEntry = 'Empate'
-        conf = 82
-        mainPattern = 'Empate Recorrente'
+    quebrar_surf = surf_detectado e current_streak >= 4
+    quebrar_zigzag = zigzag_detectado and all(r != "Empate" for r in ultimos3)
+    quebrar_duplas = duplas_repetidas
+
+    casa = recentes.count("Casa")
+    visitante = recentes.count("Visitante")
+    empate = recentes.count("Empate")
+
+    entrada = Nenhuma
+    confiança = 0
+    padrão = ""
+
+    se quebrar_surf:
+        entrada = "Visitante" if recentes[-1] == "Casa" else "Casa"
+        confiança = 88 + min(sequência_atual * 2, 10)
+        padrão = "Quebra de Surf"
+    elif empate_recorrente:
+        entrada = "Empate"
+        confiança = 82
+        padrao = "Empate Recorrente"
     elif padrao_3x1:
-        suggestedEntry = 'Visitante' if recent[-1] == 'Casa' else 'Casa'
-        conf = 80
-        mainPattern = 'Padrão 3x1'
+        entrada = "Visitante" if recentes[-1] == "Casa" else "Casa"
+        confiança = 80
+        padrão = "Padrão 3x1"
     elif espelhamento:
-        penultimo = recent[-2]
-        suggestedEntry = penultimo if penultimo != 'Empate' else 'Casa'
-        conf = 78
-        mainPattern = 'Espelhamento'
-    elif zigzagDetected:
-        lastResult = recent[-1]
-        if lastResult != 'Empate':
-            suggestedEntry = 'Visitante' if lastResult == 'Casa' else 'Casa'
-            conf = 75
-            mainPattern = 'Zig-Zag'
-    elif alternanciaEmpate:
-        suggestedEntry = 'Empate'
-        conf = 72
-        mainPattern = 'Alternância c/ Empate'
-    elif surfDetected:
-        suggestedEntry = recent[-1]
-        conf = 65 + currentStreak * 3
-        mainPattern = 'Surf Continuado'
-    elif duplasRepetidas:
-        suggestedEntry = recent[-1]
-        conf = 68
-        mainPattern = 'Duplas Repetidas'
+        entrada = recentes[-2] if recentes[-2] != "Empate" else "Casa"
+        confiança = 78
+        padrão = "Espectro"
+    elif zigzag_detectado:
+        entrada = "Visitante" if recentes[-1] == "Casa" else "Casa"
+        confiança = 75
+        padrao = "Zig-Zag"
+    elif alternancia_empate:
+        entrada = "Empate"
+        confiança = 72
+        padrao = "Alternância c/ Empate"
+    elif surf_detectado:
+        entrada = recentes[-1]
+        confianca = 65 + current_streak * 3
+        padrao = "Surf Continuado"
+    elif duplas_repetidas:
+        entrada = recentes[-1]
+        confiança = 68
+        padrao = "Duplas Repetidas"
     elif padrao_escada:
-        suggestedEntry = 'Visitante' if recent[-1] == 'Casa' else 'Casa'
-        conf = 70
-        mainPattern = 'Padrão Escada'
-    else:
-        if casaCount > visitanteCount + 2:
-            suggestedEntry = 'Visitante'
-            conf = 60
-            mainPattern = 'Análise Estatística'
-        elif visitanteCount > casaCount + 2:
-            suggestedEntry = 'Casa'
-            conf = 60
-            mainPattern = 'Análise Estatística'
-        elif empateCount == 0 and len(recent) >= 8:
-            suggestedEntry = 'Empate'
-            conf = 65
-            mainPattern = 'Falta de Empate'
-        else:
-            suggestedEntry = random.choice(['Casa', 'Visitante'])
-            conf = 50
-            mainPattern = 'Análise Básica'
+        entrada = "Visitante" if recentes[-1] == "Casa" else "Casa"
+        confiança = 70
+        padrão = "Padrão Escada"
+    outro:
+        se casa > visitante + 2:
+            entrada = "Visitante"
+            confiança = 60
+        elif visitante > casa + 2:
+            entrada = "Casa"
+            confiança = 60
+        elif empate == 0 e len(recentes) >= 8:
+            entrada = "Empate"
+            confiança = 65
+        outro:
+            entrada = random.choice(["Casa", "Visitante"])
+            confiança = 50
+        padrao = "Análise Estatística"
 
-    return {
-        'entry': suggestedEntry,
-        'confidence': min(conf, 95),
-        'mainPattern': mainPattern,
-        'patterns': {
-            'surf': surfDetected,
-            'surfStreak': currentStreak,
-            'zigzag': zigzagDetected,
-            'duplasRepetidas': duplasRepetidas,
-            'empateRecorrente': empateRecorrente,
-            'padrao_escada': padrao_escada,
-            'espelhamento': espelhamento,
-            'alternanciaEmpate': alternanciaEmpate,
-            'padrao_onda': padrao_onda,
-            'padrao_3x1': padrao_3x1,
-            'padrao_3x3': padrao_3x3,
-            'empatesPontosFixes': empatesPontosFixes,
-            'quebrarSurf': quebrarSurf,
-            'quebrarZigzag': quebrarZigzag,
-            'quebrarDuplas': quebrarDuplas
-        }
+    retornar {
+        "entrada": entrada,
+        "confianca": min(confianca, 95),
+        "padrão": padrão
     }
-    Football Studio Pro em Streamlit (parte visual)
 
-import streamlit as st
+# ---------- Interface ----------
+st.title("₂ Analisador de Estúdio de Futebol")
+st.markdown("Análise de padrões com base no histórico de resultados.")
 
-Cabeçalho
+se "historico" não estiver em st.session_state:
+    st.session_state.historico = []
 
-st.markdown(""" <h1 style='text-align: center; color: #facc15;'>⚽ Football Studio Pro</h1> <p style='text-align: center; color: #aaa;'>Análise Inteligente de Padrões - Evolution Gaming</p> """, unsafe_allow_html=True)
+col1, col2, col3 = st.columns(3)
+com col1:
+    se st.button("ðŸ Casa", use_container_width=True):
+        st.session_state.historico.append("Casa")
+com col2:
+    se st.button("ðŸ¤ Empate", use_container_width=True):
+        st.session_state.historico.append("Empate")
+com col3:
+    se st.button("âœˆï¸ Visitante", use_container_width=True):
+        st.session_state.historico.append("Visitante")
 
-Inicialização de variáveis de estado
+if st.button("ðŸ” Limpar histórico", use_container_width=True):
+    st.session_state.historico.clear()
 
-if 'history' not in st.session_state: st.session_state.history = [] if 'suggestion' not in st.session_state: st.session_state.suggestion = None if 'streak' not in st.session_state: st.session_state.streak = {'type': None, 'count': 0}
+st.markdown("### Últimos Resultados:")
+st.write(" ".join(st.session_state.historico[-20:]))
 
-Placeholder para a função analyze_patterns
-
-Você deve implementar ou importar a função analyze_patterns completa aqui
-
-def analyze_patterns(results): return None  # Substitua com a lógica real de análise de padrões
-
-Botões de entrada
-
-col1, col2, col3 = st.columns(3) with col1: if st.button("🏠 CASA"): st.session_state.history.append("Casa") with col2: if st.button("🤝 EMPATE"): st.session_state.history.append("Empate") with col3: if st.button("✈️ VISITANTE"): st.session_state.history.append("Visitante")
-
-Analisar e atualizar sugestão
-
-analysis = analyze_patterns(st.session_state.history) if analysis: st.session_state.suggestion = analysis if len(st.session_state.history) >= 1: last = st.session_state.history[-1] if st.session_state.streak['type'] == last: st.session_state.streak['count'] += 1 else: st.session_state.streak = {'type': last, 'count': 1}
-
-Exibir sugestão
-
-if st.session_state.suggestion: s = st.session_state.suggestion st.subheader(f"Padrão Principal: {s['mainPattern']}") st.markdown(f"### Próxima Entrada: {s['entry']} ({s['confidence']}% confiança)")
-
-Histórico visual
-
-st.markdown("### Histórico") row = "" for i, r in enumerate(reversed(st.session_state.history)): cor = '#ef4444' if r == 'Casa' else ('#3b82f6' if r == 'Visitante' else '#737373') letra = 'C' if r == 'Casa' else ('V' if r == 'Visitante' else 'E') row += f"<div style='width: 40px; height: 40px; background:{cor}; border-radius:50%; display:inline-flex; align-items:center; justify-content:center; margin:2px; font-weight:bold;'>{letra}</div>" st.markdown(row, unsafe_allow_html=True)
-
-Estatísticas
-
-if st.session_state.history: total = len(st.session_state.history) casa = st.session_state.history.count("Casa") empate = st.session_state.history.count("Empate") visitante = st.session_state.history.count("Visitante") st.markdown(f""" ### Estatísticas - Casa: {casa} ({(casa/total)*100:.1f}%) - Empate: {empate} ({(empate/total)*100:.1f}%) - Visitante: {visitante} ({(visitante/total)*100:.1f}%) """)
-
-Streak
-
-if st.session_state.streak['type']: st.markdown(f"### Streak Atual: {st.session_state.streak['count']}x {st.session_state.streak['type']}")
-
-Limpar histórico
-
-if st.button("Limpar Histórico"): st.session_state.history = [] st.session_state.streak = {'type': None, 'count': 0} st.session_state.suggestion = None
-
-Aviso legal
-
-st.markdown("""
-
-<div style='text-align: center; color: #aaa; font-size: small;'>
-    <p>⚠️ Este aplicativo é apenas para fins educacionais e de entretenimento.</p>
-    <p>Apostas envolvem riscos. Jogue com responsabilidade.</p>
-</div>
-""", unsafe_allow_html=True)
+se st.session_state.historico:
+    resultado = analisar_padroes(st.session_state.historico)
+    se resultado:
+        st.markdown(f"### Sugestão: **{resultado['entrada']}**")
+        st.markdown(f"Confiança: **{resultado['confianca']}%**")
+        st.markdown(f"Padrão Detectado: _{resultado['padrão']}_")
